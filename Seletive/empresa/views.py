3a8ead52b2a8c77c1726f43648414ab097b333f5
Victor Alecrim <victoralecrim11@gmail.com>
2022-12-01 22:47:58 -0300

    listagem de empresas

--- Seletive/empresa/views.py
+++ Seletive/empresa/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Tecnologias
+from .models import Tecnologias, Empresa
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.messages import constants
 # Create your views here.
 
 
@@ -17,3 +20,40 @@ def nova_empresa(request):
         caracteristicas = request.POST.get('caracteristicas')
         tecnologias = request.POST.getlist('tecnologias')
         logo = request.FILES.get('logo')
+
+    # Validações do formulario
+
+    if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)):
+        messages.add_message(request, constants.ERROR,
+                             'Preencha todos os campos')
+        return redirect('/home/nova_empresa')
+
+    if logo.size > 100_000_000:
+        messages.add_message(request, constants.ERROR,
+                             'Preencha todos os campos')
+        return redirect('/home/nova_empresa')
+
+    if nicho not in [i[0] for i in Empresa.choices_nicho_mercado]:
+        messages.add_message(request, constants.ERROR,
+                             'Preencha todos os campos')
+        return redirect('/home/nova_empresa')
+
+    empresa = Empresa(logo=logo,
+                      nome=nome,
+                      email=email,
+                      cidade=cidade,
+                      endereco=endereco,
+                      nicho_mercado=nicho,
+                      caracteristica_empresa=caracteristicas)
+    empresa.save()
+    empresa.tecnologias.add(*tecnologias)
+    empresa.save()
+    messages.add_message(request, constants.SUCCESS,
+                         'Empresa cadastrada com sucesso')
+    return redirect('/home/nova_empresa')
+
+
+def empresas(request):
+    empresas = Empresa.objects.all()
+    tecnologias = Tecnologias.objects.all()
+    return render(request, 'empresa.html', {'empresas': empresas, 'tecnologias': tecnologias})
